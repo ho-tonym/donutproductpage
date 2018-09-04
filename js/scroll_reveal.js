0038eb2cf9358f3274539b10764cf54334f9dee1
ho-tonym <ho.tonym@gmail.com>
2018-09-03 21:46:31 -0400

    replace old stock images with web optimized ones. logo and donut vectors still needvectors still need to be replaced

---
+++ js/scroll_reveal.js
@@ -0,0 +1,93 @@
+window.sr = ScrollReveal();
+// 1
+sr.reveal('._sr_1_text_1', {
+  viewFactor: 0.2,
+  duration: 1800,
+  origin:'bottom',
+  distance:'5px',
+  delay: 400
+});
+
+sr.reveal('._sr_1_text_2', {
+  viewFactor: 0.2,
+  duration: 1800,
+  origin:'bottom',
+  distance:'5px',
+  delay: 800
+});
+
+sr.reveal('._sr_1_img', {
+  viewFactor: 0.2,
+  duration: 1800,
+  origin:'bottom',
+  distance:'5px'
+});
+// 2
+sr.reveal('._sr_2_1_img', {
+  viewFactor: 0.2,
+  duration: 1800,
+  origin:'bottom',
+  distance:'25px'
+});
+
+sr.reveal('._sr_2_2_img', {
+  viewFactor: 0.2,
+  duration: 1800,
+  delay: 150,
+  origin:'bottom',
+  distance:'25px'
+});
+
+sr.reveal('._sr_2_3_img', {
+  viewFactor: 0.2,
+  duration: 1800,
+  delay: 300,
+  origin:'bottom',
+  distance:'25px'
+});
+//3
+sr.reveal('._sr_3_1_img', {
+  viewFactor: 0.1,
+  duration: 1800,
+  origin:'bottom',
+  distance:'25px'
+});
+sr.reveal('._sr_3_2_img', {
+  viewFactor: 0.1,
+  duration: 1800,
+  origin:'bottom',
+  distance:'25px'
+});
+sr.reveal('._sr_3_3_img', {
+  viewFactor: 0.1,
+  duration: 1800,
+  origin:'bottom',
+  distance:'25px'
+});
+sr.reveal('._sr_3_4_img', {
+  viewFactor: 0.1,
+  duration: 1800,
+  origin:'bottom',
+  distance:'25px'
+});
+//4
+sr.reveal('._sr_4_1_img', {
+  viewFactor: 0.1,
+  duration: 1800,
+  origin:'bottom',
+  distance:'10px'
+});
+sr.reveal('._sr_4_2_img', {
+  viewFactor: 0.1,
+  duration: 1800,
+  origin:'bottom',
+  distance:'10px',
+  delay: 150
+});
+sr.reveal('._sr_4_3_img', {
+  viewFactor: 0.1,
+  duration: 1800,
+  origin:'bottom',
+  distance:'10px',
+  delay: 300
+});
