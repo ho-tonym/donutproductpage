9e5d8e67111512b158fb2213cf7896e6e32af938
ho-tonym <ho.tonym@gmail.com>
2018-09-28 20:16:26 -0400

    break up section 2 animation into 3 parts for each div so there is a slight parallex effect

--- js/scroll_reveal.js
+++ js/scroll_reveal.js
@@ -29,7 +29,19 @@ sr.reveal('._sr_2_1_img', {
   origin:'bottom',
   distance:'25px'
 });
-
+sr.reveal('._sr_2_1_h2', {
+  viewFactor: 0.2,
+  duration: 2200,
+  origin:'bottom',
+  distance:'30px'
+});
+sr.reveal('._sr_2_1_p', {
+  viewFactor: 0.2,
+  duration: 3000,
+  origin:'bottom',
+  distance:'35px'
+});
+//
 sr.reveal('._sr_2_2_img', {
   viewFactor: 0.2,
   duration: 1800,
@@ -37,13 +49,37 @@ sr.reveal('._sr_2_2_img', {
   origin:'bottom',
   distance:'25px'
 });
-
+sr.reveal('._sr_2_2_h2', {
+  viewFactor: 0.2,
+  duration: 2200,
+  origin:'bottom',
+  distance:'30px'
+});
+sr.reveal('._sr_2_2_p', {
+  viewFactor: 0.2,
+  duration: 3000,
+  origin:'bottom',
+  distance:'35px'
+});
+//
 sr.reveal('._sr_2_3_img', {
   viewFactor: 0.2,
   duration: 1800,
   delay: 300,
   origin:'bottom',
   distance:'25px'
+});
+sr.reveal('._sr_2_3_h2', {
+  viewFactor: 0.2,
+  duration: 2200,
+  origin:'bottom',
+  distance:'30px'
+});
+sr.reveal('._sr_2_3_p', {
+  viewFactor: 0.2,
+  duration: 3000,
+  origin:'bottom',
+  distance:'35px'
 });
 //3
 sr.reveal('._sr_3_1_img', {
