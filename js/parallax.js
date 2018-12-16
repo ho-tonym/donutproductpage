a787998c8631c25210714275c7eaed6e0c1e1c5c
ho-tonym <ho.tonym@gmail.com>
2018-12-15 19:59:30 -0500

    issue with scrollreveal library - making a call that takes a long time slowing down the page

--- js/parallax.js
+++ js/parallax.js
@@ -2,7 +2,6 @@ const parallax = document.getElementById("s1");
 
 window.addEventListener("scroll", function(){
   let offset = window.pageYOffset;
-  let scrollAmt = 0.6;
-  console.log(offset);
+  let scrollAmt = 0.1;
   parallax.style.backgroundPositionY = offset * scrollAmt + "px";
 })
