178002a61a7bf40e322add0e09707a03de98d20b
ho-tonym <ho.tonym@gmail.com>
2018-12-15 22:45:15 -0500

    trickle down transition property in universal selector

--- js/parallax.js
+++ js/parallax.js
@@ -2,6 +2,6 @@ const parallax = document.getElementById("s1");
 
 window.addEventListener("scroll", function(){
   let offset = window.pageYOffset;
-  let scrollAmt = 0.1;
+  let scrollAmt = 0.5;
   parallax.style.backgroundPositionY = offset * scrollAmt + "px";
 })
